0898d4ad96d4db0f0be668961476e7368249852f
luzhao <luzhao@luzhaodeMacBook-Pro.local>
2018-03-16 11:34:06 +0800

    add render question code

--- public/assets/js/view/exercise.js
+++ public/assets/js/view/exercise.js
@@ -39,9 +39,8 @@ $(function () {
   if (emotion == 4)  html.push('<a id="general" class="btn btn-success  btn-sm">General Mode</a>');
   $('.mood').html(html.join(''));
 
-  var point =0;
-  html.push('<h3><labe1 class="font-point">' + point + '</labe1></h3>');
-  $('.point').html(point.join(''));
+  var point = 0;
+  $('.point').html('<h3><labe1 class="font-point">' + point + '</labe1></h3>');
 
   var i = 0;
   html = [];
@@ -75,4 +74,212 @@ $(function () {
   }
   $('.question-group-two').html(html.join(''));
 
+  var question = [
+    {
+      value: '1',
+      style: {
+        backgroundColor: '#e67e22'
+      }
+    },
+    {
+      value: '2',
+      style: {
+        backgroundColor: '#1ABC9C'
+      }
+    },
+    {
+      value: '3',
+      style: {
+        backgroundColor: '#f1c40f'
+      }
+    },
+    {
+      value: '4',
+      style: {
+        backgroundColor: '#2ECC71'
+      }
+    },
+    {
+      value: '5',
+      style: {
+        backgroundColor: '#3498DB'
+      }
+    },
+    {
+      value: '6',
+      style: {
+        backgroundColor: '#E74C3C'
+      }
+    },
+    {
+      value: '7',
+      style: {
+        backgroundColor: '#9B59B6'
+      }
+    },
+    {
+      value: '8',
+      style: {
+        backgroundColor: '#BDC3C7'
+      }
+    },
+    {
+      value: '9',
+      style: {
+        backgroundColor: '#447788'
+      }
+    },
+    {
+      value: '0',
+      style: {
+        backgroundColor: '#203537'
+      }
+    }
+  ];
+  var operation = [
+    {
+      value: '÷',
+      style: {
+        backgroundColor: '#3498DB'
+      }
+    },
+    {
+      value: '×',
+      style: {
+        backgroundColor: '#E74C3C'
+      }
+    },
+    {
+      value: '-',
+      style: {
+        backgroundColor: '#9B59B6'
+      }
+    },
+    {
+      value: '+',
+      style: {
+        backgroundColor: '#BDC3C7'
+      }
+    }
+  ];
+  var equal = [
+    {
+      value: '=',
+      style: {
+        backgroundColor: '#2ECC71'
+      }
+    }
+  ];
+  var answer = [
+    {
+      value: '?',
+      style: {
+        backgroundColor: '#e67e22'
+      }
+    },
+    {
+      value: '1',
+      style: {
+        backgroundColor: '#e67e22'
+      }
+    },
+    {
+      value: '2',
+      style: {
+        backgroundColor: '#1ABC9C'
+      }
+    },
+    {
+      value: '3',
+      style: {
+        backgroundColor: '#f1c40f'
+      }
+    },
+    {
+      value: '4',
+      style: {
+        backgroundColor: '#2ECC71'
+      }
+    },
+    {
+      value: '5',
+      style: {
+        backgroundColor: '#3498DB'
+      }
+    },
+    {
+      value: '6',
+      style: {
+        backgroundColor: '#E74C3C'
+      }
+    },
+    {
+      value: '7',
+      style: {
+        backgroundColor: '#9B59B6'
+      }
+    },
+    {
+      value: '8',
+      style: {
+        backgroundColor: '#BDC3C7'
+      }
+    },
+    {
+      value: '9',
+      style: {
+        backgroundColor: '#447788'
+      }
+    },
+    {
+      value: '0',
+      style: {
+        backgroundColor: '#203537'
+      }
+    }
+  ];
+
+  //var submit = document.getElementById('submit');
+  //var output = document.getElementById('output');
+  var questionCube = new HexaFlip(document.getElementById('hexaflip-demo5'),
+    { set1: question, set2: question, set3: operation, set4: question, set5: question, set6: equal, set7: answer, set8: answer },
+    { fontSize: 100, margin: 4, perspective: 1000 }
+  );
+
+  var currentQuestion = 1;
+  var QUESTION_COUNT = 10;
+
+  var qreg = /(\d)?(\d)([+-×÷])(\d)?(\d)/;
+  var qResult = Paper[i]['q' + currentQuestion].match(qreg);
+  questionCube.setValue({ set1: qResult[1] || '0', set2: qResult[2], set3: qResult[3], set4: qResult[4] || '0', set5: qResult[5], set6: '=',  set7: '?', set8: '?' });
+
+
+  /*
+   var one = document.getElementById('tens-place'),
+   ten = document.getElementById('ones-plae'),
+
+   document.getElementById('set-button').addEventListener('click', function(){
+   var time = {
+   set5: one.value,
+   set6: ten.value,
+   }
+   hexaDemo5.setValue(time);
+   });
+   */
+
+  //submit.addEventListener('click', function () {
+  //  if (hexaDemo5.getValue().join(' ') === 'red yellow blue green') {
+  //    output.innerHTML = 'password correct!';
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //    hexaDemo5.flip();
+  //  } else {
+  //    output.innerHTML = 'password incorrect.';
+  //  }
+  //}, false);
 });
