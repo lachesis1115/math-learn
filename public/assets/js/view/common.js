96ef70afd3cf71abc1c1b7c2f6cc8470f857416d
lachesis1115 <15012503420@163.com>
2018-03-20 22:43:50 +0800

    refresh problem and others

--- public/assets/js/view/common.js
+++ public/assets/js/view/common.js
@@ -5,6 +5,13 @@ $(function() {
     location.href = 'login.html';
   }
   $('#user-name').text(user.name);
+ 
+  if(user.name=="Richel")  $('.img-circle').attr("src","assets/img/kids/girl-6.png");
+  if(user.name=="Marcel")  $('.img-circle').attr("src","assets/img/kids/boy-6.png");
+  if(user.name=="Bill")  $('.img-circle').attr("src","assets/img/kids/boy-1.png");
+  if(user.name=="Tony")  $('.img-circle').attr("src","assets/img/kids/boy-2.png");
+  if(user.name=="Amy")  $('.img-circle').attr("src","assets/img/kids/girl-3.png");
+  
 
   $('.logout').on('click', function() {
     Cookies.remove('loginUser');
--- public/assets/js/view/exercise.js
+++ public/assets/js/view/exercise.js
@@ -8,10 +8,10 @@ $(function () {
     var sclevel = parseFloat($('#sc-input').val().trim());
     var bvp = parseFloat($('#bvp-input').val().trim());
 
-    if (heartRate && typeof heartRate === 'number' && heartRate >= 60 && heartRate <= 90) {
+    if (heartRate && typeof heartRate === 'number' && heartRate >= 60 && heartRate <= 100) {
       LoginUser.heartRate = heartRate;
     } else {
-      return alert('the heartRate value should between 60-90!');
+      return alert('the heartRate value should between 60-100!');
     }
     if (sclevel && typeof sclevel === 'number' && sclevel >= 0 && sclevel <= 100) {
       LoginUser.sclevel = sclevel;
@@ -25,11 +25,24 @@ $(function () {
     }
     Cookies.set('loginUser', LoginUser);
     $('#myModal').modal('hide');
+
+    var emotion = 1;
+    if (LoginUser.bvp > LoginUser.heartRate && LoginUser.bvp > LoginUser.sclevel)  emotion = 3;
+    if (LoginUser.sclevel > LoginUser.heartRate && LoginUser.sclevel > LoginUser.bvp) emotion = 2;
+    if (LoginUser.heartRate > LoginUser.sclevel && LoginUser.heartRate > LoginUser.bvp)  emotion = 1;
+    if (LoginUser.heartRate == LoginUser.sclevel && LoginUser.heartRate == LoginUser.bvp) emotion = 4;
+  
+    var html = [];
+    if (emotion == 3)  html.push('<a id="bvp" class="btn btn-lg  btn-info">Happy Mode</a>');
+    if (emotion == 1)  html.push('<a id="heartRate" class="btn btn-lg  btn-warning">Sad Mode</a>');
+    if (emotion == 2)  html.push('<a id="sclevel" class="btn btn-lg  btn-danger">Exciting Mode</a>');
+    if (emotion == 4)  html.push('<a id="general" class="btn btn-lg  btn-success">General Mode</a>');
+    $('.mood').html(html.join(''));
   });
 
   $('#music-switch').on('change', function() {
     var mSwitch = $(this)[0].checked;
-    var bgm = $('#bgm')[0];
+    var bgm = $('#bgm1')[0];
     if (mSwitch) {
       bgm.play();
     } else {
@@ -37,19 +50,23 @@ $(function () {
     }
   });
 
-  var emotion = 1;
-  if (LoginUser.bvp > LoginUser.heartRate && LoginUser.bvp > LoginUser.sclevel)  emotion = 3;
-  if (LoginUser.sclevel > LoginUser.heartRate && LoginUser.sclevel > LoginUser.bvp) emotion = 2;
-  if (LoginUser.heartRate > LoginUser.sclevel && LoginUser.heartRate > LoginUser.bvp)  emotion = 1;
-  if (LoginUser.heartRate == LoginUser.sclevel && LoginUser.heartRate == LoginUser.bvp) emotion = 4;
+  $('#music1').on('click', function() {
+    var bgm = $('#bgm1')[0];
+    bgm.play();
 
-  var html = [];
-  if (emotion == 3)  html.push('<a id="bvp" class="btn btn-lg  btn-info  btn-sm">bvp Mode</a>');
-  if (emotion == 1)  html.push('<a id="heartRate" class="btn btn-lg  btn-warning  btn-sm">heartRate Mode</a>');
-  if (emotion == 2)  html.push('<a id="sclevel" class="btn btn-lg  btn-danger  btn-sm">sclevel Mode</a>');
-  if (emotion == 4)  html.push('<a id="general" class="btn btn-lg  btn-success  btn-sm">General Mode</a>');
-  $('.mood').html(html.join(''));
+  });
 
+  $('#music2').on('click', function() {
+    var bgm = $('#bgm2')[0];
+    bgm.play();
+
+  });
+
+  $('#music3').on('click', function() {
+    var bgm = $('#bgm3')[0];
+    bgm.play();
+
+  });
 
   //exercise cube part
   var question = [
@@ -221,15 +238,27 @@ $(function () {
   var questionNumber = 1;
   var QUESTION_COUNT = 10;
   var correctNumber = 0;
-  var accuracy = 0;
   var global_value = 0;
   var i = 0;
+  var combo = 0;
+  var accuracy=0;
+
+  $('#select_exam').on('click', function() {
+    var options=$("#select option:selected");
+    i=options.val()-1;
+    refresh();
+  });
+
+  function refresh(){
+    window.location.reload();
+  }
 
   //initial part
-  $('.point').html('<h3><label class="font-point">' + point + '</label></h3>');
-  $('.title').html('<h3><labe1 class="font-title">' + Paper[i]["title"] + '</label></h3>');
-  $('.level').html('<h3><labe1 class="font-title">' + Paper[i]["level"] + '</label></h3>');
-  $('.question_number').html('<h3><labe1 class="font-point">' + questionNumber + '</labe1></h3>');
+  $('.point').html('<h4><label class="font-point">' + point + '</label></h4>');
+  $('.title').html('<h4><labe1 class="font-title">' + Paper[i]["title"] + '</label></h4>');
+  $('.level').html('<h4><labe1 class="font-title">' + Paper[i]["level"] + '</label></h4>');
+  $('.question_number').html('<h4><labe1 class="font-title">' + questionNumber + '</labe1></h4>');
+  $('.combo').html('<h4><labe1 class="font-point">' + combo + '</label></h4>');
 
   var questionCube = new HexaFlip(document.getElementById('hexaflip-demo5'),
     {
@@ -272,20 +301,48 @@ $(function () {
     var html = [];
 
     if (global_value == Paper[i]['a' + questionNumber]) {
+      $('#bgm_correct')[0].play();
       $('.judge').html('<h2 class="mb right">&#10004</h2>');
       point = point + 10;
       html = [];
-      $('.point').html('<h3><labe1 class="font-point">' + point + '</labe1></h3>');
       correctNumber = correctNumber + 1;
+      combo = combo+1;
+      $('.combo').html('<h4><labe1 class="font-point">' + combo + '</label></h4>');
+      if(combo==3){
+          $('#bgm_un')[0].play();
+          $('.alert_show').html('<img src="assets/img/kids/girl-6.png" height="60" width="60"/><label class="alert_info"><strong>Excellent! </strong>point+10</label>');
+          point=point+10;
+      }
+      if(combo==5){
+          $('#bgm_un')[0].play();
+          $('.alert_show').html('<img src="assets/img/kids/girl-6.png" height="60" width="60"/><label class="alert_info"><strong>Unbelievable! </strong>point+20 </label>');
+          point=point+20;
+      }
+      if(combo==10){
+          $('#bgm_un')[0].play();
+          $('.alert_show').html('<img src="assets/img/kids/girl-6.png" height="60" width="60"/><label class="alert_info"><strong>Perfect work! </strong>point+50 </label>');
+          point=point+50;
+      }
+      $('.point').html('<h4><labe1 class="font-point">' + point + '</labe1></h4>');
+      if(questionNumber==10){
+        $('#bgm_complete')[0].play();
+        $('.alert_show').html('<label class="alert_info"><strong>Congratulations! </strong>You have done all the questions!<a id="see_report" data-toggle="modal" data-target="#report">See the report here.</a</label>');
+      }
+      
     }
     else {
+      $('#bgm_wrong')[0].play();
       $('.judge').html('<h2 class="mb wrong">&#10007</h2>');
+      combo=0;
+      $('.combo').html('<h4><labe1 class="font-point">' + combo + '</label></h4>');
     }
   });
 
   $('#next').on('click', function () {
+    $('#bgm_click')[0].play();
     questionNumber = questionNumber + 1;
     $('.judge').html('');
+    $("#answer_text").val("");
     qResult = Paper[i]['q' + questionNumber].match(qreg);
     questionCube.setValue({
       set1: qResult[1] || '0',
@@ -297,42 +354,29 @@ $(function () {
       set7: '?',
       set8: '?'
     });
-    $('.question_number').html('<h3><labe1 class="font-title">' + questionNumber + '</labe1></h3>');
+    $('.question_number').html('<h4><labe1 class="font-title">' + questionNumber + '</labe1></h4>');
     if (questionNumber == 10) {
-      $('.info').html('<strong>Wonderful!</strong> You have done all the questions!<a data-toggle="modal" data-target="#report"> See the report here</a></h3>');
+      $('.info').html('<strong>Wonderful!</strong> You have done all the questions!<a data-toggle="modal" data-target="#report"> See the report here</a></h4>');
 
     }
   });
 
-  accuracy = correctNumber / questionNumber;
+  $('#see_report').on('click', function () {
+    accuracy=correctNumber*10;
+    var doughnutData = [
+      {
+        value: accuracy,
+        color: "#31aa33"
+      },
+      {
+        value: 100-accuracy,
+        color: "#fdfdfd"
+      }
+    ];
+    var myDoughnut = new Chart(document.getElementById("report_canvas").getContext("2d")).Doughnut(doughnutData);
+    $('.acc').html('<h4>' + accuracy + '</h4>');
+    $('#point').html('<h4>' + point + '</h4>');
+  });
 
-  //jQuery(".fancybox").fancybox();
-
-
-  var doughnutData = [
-    {
-      value: 70,
-      color: "#31aa33"
-    },
-    {
-      value: 30,
-      color: "#fdfdfd"
-    }
-  ];
-  var myDoughnut = new Chart(document.getElementById("report_canvas").getContext("2d")).Doughnut(doughnutData);
-
+  
 });
-
-function two_char(n) {
-  return n >= 10 ? n : "0" + n;
-}
-function time_fun() {
-  var sec = 0;
-  setInterval(function () {
-    sec++;
-    var date = new Date(0, 0)
-    date.setSeconds(sec);
-    var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
-    document.getElementById("time").innerText = two_char(h) + ":" + two_char(m) + ":" + two_char(s);
-  }, 1000);
-}
